a237113d2bdd17b2f372769c62bdb0af148e7b31
lahiruharshana <lharshana2002@gmail.com>
2025-08-20 14:17:21 +0530

    done supplier crud

--- src/main/java/com/erp/inventory/system/repository/SupplierRepository.java
+++ src/main/java/com/erp/inventory/system/repository/SupplierRepository.java
@@ -5,6 +5,10 @@ import com.erp.inventory.system.model.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SupplierRepository extends JpaRepository<Supplier, Long> {
+    Optional<Supplier> findByEmail(String email);
+
 }
